aa9b36d2f50c2e2126f5897893328a0202e3eeac
狐璃 <zhilaohu114514@163.com>
2023-10-18 13:12:43 +0000

    guoba.support.js
    Signed-off-by: 狐璃 <zhilaohu114514@163.com>

---
+++ guoba.support.js
@@ -0,0 +1,23 @@
+// 支持锅巴
+export function supportGuoba() {
+  return {
+    pluginInfo: {
+      name: 'hl', //插件名字
+      title: 'HL-Plugin', //插件标题
+      author: '@狐璃', //作者名字（直接加在@符号后面就行了）
+      authorLink: 'https://gitee.com/fox-glaze', //作者的Git主页链接（博客也行）
+      link: 'https://gitee.com/fox-glaze', //插件的Git链接（写错了就读取不到md）
+      isV3: true, //支持V3
+      isV2: true, //支持V2
+      description: '缝缝补补的小插件', //插件说明（显示在名字下面的那行字）
+      icon: 'svg-spinners:gooey-balls-2', //图标 可在这个网站里找 https://icon-sets.iconify.design （直接复制图片的名字就行）
+      iconColor: 'rgb(135, 206, 235)' //图标颜色 （我只会用RGB 这个也方便一点）
+    },
+      }
+    }
+/**
+其他配置去哪了？ 我不会写啊
+https://gitee.com/guoba-yunzai/guoba-plugin/blob/master/guoba.support.js
+看锅巴插件的示例吧
+我连配置文件都不会写
+*/

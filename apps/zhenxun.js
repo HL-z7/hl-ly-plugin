5bd5bcf1322e63011b0ed7c1a5c206b0cd0418f1
狐璃 <zhilaohu114514@163.com>
2023-10-19 12:54:50 +0000

    update apps/zhenxun.js.
    Signed-off-by: 狐璃 <zhilaohu114514@163.com>

--- apps/zhenxun.js
+++ apps/zhenxun.js
@@ -18,7 +18,7 @@ export class example extends plugin {
   }
   async zhenxun (e) {
     logger.info('[zhenxunHomo.js插件]')
-    let url = encodeURI(`https://hanxuan.cc/zhenxun.mp4`)
+    let url = encodeURI(`https://img.kookapp.cn/attachments/2023-10/19/653126dc5fda6.mp4`)
     await this.e.reply(segment.video(url))
   }
 }

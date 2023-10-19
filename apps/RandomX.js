2267af3657466027fb61bf100fe412ec98334159
狐璃 <zhilaohu114514@163.com>
2023-10-19 04:26:01 +0000

    update apps/RandomX.js.
    Signed-off-by: 狐璃 <zhilaohu114514@163.com>

--- apps/RandomX.js
+++ apps/RandomX.js
@@ -21,7 +21,7 @@ export class RandomX extends plugin {
             rule: [
                 {
                     reg: null,  // 正则表达式初始化为空字符串，稍后从配置文件中读取
-                    fnc: 'h'   // 定义函数名为h
+                    fnc: 'hhh'   // 定义函数名为h
                 }
             ]
         });
@@ -37,7 +37,7 @@ export class RandomX extends plugin {
         this.rule[0].reg = new RegExp(`(${reg.join('|')})`);
 
         // 设置图片文件夹路径
-        this.imageDirPath = path.join(__dirname, 'plugins/hl-pluginl/resources/h'); // 图片文件夹路径
+        this.imageDirPath = path.join(__dirname, 'plugins/hl-pluginl/resources/hhh'); // 图片文件夹路径
     }
 
     // 定义处理函数h

a6682767fded1f15ae3ae9776fc3ed907ccc1976
落雨天阳 <923276093@qq.com>
2023-10-21 12:58:24 +0000

    发柴郡就会触发有关柴郡的表情包
    Signed-off-by: 落雨天阳 <923276093@qq.com>

---
+++ apps/随机柴郡.js
@@ -0,0 +1,26 @@
+﻿import plugin from '../../../lib/plugins/plugin.js'
+import fs from 'node:fs'
+
+export class example extends plugin {
+  constructor () {
+    super({
+      name: '随机柴郡',
+      dsc: '随机柴郡',
+      event: 'message',
+      priority: -114514,
+      rule: [
+        {
+          reg: '柴郡',
+          fnc: 'cj'
+        }
+      ]
+    })
+  }
+  
+  async cs (e) {
+    logger.info('[随机柴郡.js插件]')
+    let url = encodeURI(`https://api.lolimi.cn/API/chaiq/c.php`)
+    await this.e.reply(segment.image(url), true,{at:true})
+    return true
+  }
+}

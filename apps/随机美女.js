488558cb3b1f4fafab64d0cbdb595ef37ffa2711
落雨天阳 <923276093@qq.com>
2023-10-21 13:00:00 +0000

    小姐姐图片你值得拥有，温馨提示：小心虚了
    Signed-off-by: 落雨天阳 <923276093@qq.com>

---
+++ apps/随机美女.js
@@ -0,0 +1,26 @@
+﻿import plugin from '../../../lib/plugins/plugin.js'
+import fs from 'node:fs'
+
+export class example extends plugin {
+  constructor () {
+    super({
+      name: '随机美女',
+      dsc: '随机美女',
+      event: 'message',
+      priority: -114514,
+      rule: [
+        {
+          reg: '^#?(美女|小姐姐)$',
+          fnc: 'sjmn'
+        }
+      ]
+    })
+  }
+  
+  async cs (e) {
+    logger.info('[随机美女.js插件]')
+    let url = encodeURI(`https://api.lolimi.cn/API/tup/xjj.php`)
+    await this.e.reply(segment.image(url), true,{at:true})
+    return true
+  }
+}

c600a32c9b411743ba85247acee3b44ffb3c6e32
落雨天阳 <923276093@qq.com>
2023-10-22 06:58:24 +0000

    帮助
    Signed-off-by: 落雨天阳 <923276093@qq.com>

--- apps/help.js
+++ apps/help.js
@@ -1,18 +1,72 @@
-import Help from './help/Help.js'
-import { App } from '#hl'
+import { plugin } from '../../../lib/plugins/plugin.js';
+import * as lodash from 'lodash';
+import { Common, Data } from '../components/index.js';
+import Theme from './help/theme.js';
 
-let app = App.init({
-  id: 'help',
-  name: 'hl',
-  desc: '喵喵帮助'
-})
-
-app.reg({
-  help: {
-    rule: /^#?hl帮助|hl菜单|hlhelp|hl功能|hl指令$/,
-    fn: Help.render,
-    desc: '【#帮助】 #hl帮助'
+export class xiaofei_help extends plugin {
+  constructor() {
+    super({
+      /** 功能名称 */
+      name: 'hl插件_帮助',
+      /** 功能描述 */
+      dsc: '',
+      /** https://oicqjs.github.io/oicq/#events */
+      event: 'message',
+      /** 优先级，数字越小等级越高 */
+      priority: 2000,
+      rule: [
+        {
+          /** 命令正则匹配 */
+          reg: '^#?hl(插件)?帮助$',
+          /** 执行方法 */
+          fnc: 'message'
+        }
+      ]
+    });
   }
-})
 
-export default app
+  async message() {
+    return await help(this.e);
+  }
+}
+
+async function help(e) {
+  let custom = {};
+  let help = {};
+
+  let { diyCfg, sysCfg } = await Data.importCfg('help');
+
+  custom = help;
+
+  let helpConfig = lodash.defaults(diyCfg.helpCfg || {}, custom.helpCfg, sysCfg.helpCfg);
+  let helpList = diyCfg.helpList || custom.helpList || sysCfg.helpList;
+  let helpGroup = [];
+
+  lodash.forEach(helpList, (group) => {
+    if (group.auth && group.auth === 'master' && !e.isMaster) {
+      return true;
+    }
+
+    lodash.forEach(group.list, (help) => {
+      let icon = help.icon * 1;
+      if (!icon) {
+        help.css = 'display:none';
+      } else {
+        let x = (icon - 1) % 10;
+        let y = (icon - x - 1) / 10;
+        help.css = `background-position:-${x * 50}px -${y * 50}px`;
+      }
+    });
+
+    helpGroup.push(group);
+  });
+
+  let themeData = await Theme.getThemeData(diyCfg.helpCfg || {}, sysCfg.helpCfg || {});
+
+  return await Common.render('help/index', {
+    helpCfg: helpConfig,
+    helpGroup,
+    ...themeData,
+    element: 'default'
+  }, { e, scale: 1 });
+}

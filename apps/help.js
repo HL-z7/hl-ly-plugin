9087c5d4297bfa44245370b1c9a4d03add15b4f1
落雨天阳 <923276093@qq.com>
2023-10-22 07:07:00 +0000

    update apps/help.js.
    Signed-off-by: 落雨天阳 <923276093@qq.com>

--- apps/help.js
+++ apps/help.js
@@ -1,13 +1,13 @@
-import { plugin } from '../../../lib/plugins/plugin.js';
-import * as lodash from 'lodash';
-import { Common, Data } from '../components/index.js';
-import Theme from './help/theme.js';
+import plugin from '../../../lib/plugins/plugin.js'
+import lodash from 'lodash'
+import { Common, Data } from '../components/index.js'
+import Theme from './help/theme.js'
 
 export class hl_help extends plugin {
   constructor() {
     super({
       /** 功能名称 */
-      name: 'hl插件_帮助',
+      name: '狐璃_帮助',
       /** 功能描述 */
       dsc: '',
       /** https://oicqjs.github.io/oicq/#events */
@@ -28,45 +28,45 @@ export class hl_help extends plugin {
   async message() {
     return await help(this.e);
   }
+
 }
 
 async function help(e) {
-  let custom = {};
-  let help = {};
+  let custom = {}
+  let help = {}
 
-  let { diyCfg, sysCfg } = await Data.importCfg('help');
+  let { diyCfg, sysCfg } = await Data.importCfg('help')
 
-  custom = help;
+  custom = help
 
-  let helpConfig = lodash.defaults(diyCfg.helpCfg || {}, custom.helpCfg, sysCfg.helpCfg);
-  let helpList = diyCfg.helpList || custom.helpList || sysCfg.helpList;
-  let helpGroup = [];
+  let helpConfig = lodash.defaults(diyCfg.helpCfg || {}, custom.helpCfg, sysCfg.helpCfg)
+  let helpList = diyCfg.helpList || custom.helpList || sysCfg.helpList
+  let helpGroup = []
 
   lodash.forEach(helpList, (group) => {
     if (group.auth && group.auth === 'master' && !e.isMaster) {
-      return true;
+      return true
     }
 
     lodash.forEach(group.list, (help) => {
-      let icon = help.icon * 1;
+      let icon = help.icon * 1
       if (!icon) {
-        help.css = 'display:none';
+        help.css = 'display:none'
       } else {
-        let x = (icon - 1) % 10;
-        let y = (icon - x - 1) / 10;
-        help.css = `background-position:-${x * 50}px -${y * 50}px`;
+        let x = (icon - 1) % 10
+        let y = (icon - x - 1) / 10
+        help.css = `background-position:-${x * 50}px -${y * 50}px`
       }
-    });
+    })
 
-    helpGroup.push(group);
-  });
-
-  let themeData = await Theme.getThemeData(diyCfg.helpCfg || {}, sysCfg.helpCfg || {});
+    helpGroup.push(group)
+  })
+  let themeData = await Theme.getThemeData(diyCfg.helpCfg || {}, sysCfg.helpCfg || {})
 
   return await Common.render('help/index', {
     helpCfg: helpConfig,
     helpGroup,
     ...themeData,
     element: 'default'
-  }, { e, scale: 1 });
+  }, { e, scale: 1 })
 }

3924beebd5b93a156e6eda5cb57a63754184563a
落雨天阳 <923276093@qq.com>
2023-10-23 12:57:47 +0000

    update components/common-lib / render.js.
    Signed-off-by: 落雨天阳 <923276093@qq.com>

--- components/common-lib / render.js
+++ components/common-lib / render.js
@@ -25,7 +25,7 @@ export default async function (path, params, cfg) {
     },
     sys: {
       scale: 1,
-      copyright: `Created By Yunzai-Bot<span class="version">${Version.yunzai}</span> & xiaofei-Plugin<span class="version">${Version.ver}</span>`
+      copyright: `Created By Yunzai-Bot<span class="version">${Version.yunzai} 🤖</span> & xiaofei-Plugin<span class="version">${Version.ver} 🧩</span>`
     },
     quality: 100
   }

128a5984a39f125af11fb72945991532201364c8
落雨天阳 <923276093@qq.com>
2023-10-23 12:12:08 +0000

    update apps/help.js.
    Signed-off-by: 落雨天阳 <923276093@qq.com>

--- apps/help.js
+++ apps/help.js
@@ -1,82 +1,77 @@
-import plugin from '../../../lib/plugins/plugin.js'
-import lodash from 'lodash'
-import { Cfg, Version, Common, Data } from '../components/index.js'
-import Theme from './help/help.js'
+import plugin from '../../../lib/plugins/plugin.js';
+import lodash from 'lodash';
+import { Cfg, Version, Common, Data, HelpTheme } from '../components/index.js';
+import Theme from './help/help.js';
 
 export class hl_help extends plugin {
   constructor() {
     super({
-      /** 功能名称 */
       name: '狐璃_帮助',
-      /** 功能描述 */
       dsc: '',
-      /** https://oicqjs.github.io/oicq/#events */
       event: 'message',
-      /** 优先级，数字越小等级越高 */
       priority: 2000,
       rule: [
         {
-          /** 命令正则匹配 */
           reg: '^#?hl(插件)?帮助$',
-          /** 执行方法 */
           fnc: 'message'
         }
       ]
     });
   }
 
-async help (e) {
+  async help(e) {
+    let custom = {};
+    let help = {};
+    let { diyCfg, sysCfg } = await Data.importCfg('help');
 
-  let custom = {}
-  let help = {}
-  let { diyCfg, sysCfg } = await Data.importCfg('help')
-
-  if (lodash.isArray(help.helpCfg)) {
-    custom = {
-      helpList: help.helpCfg,
-      helpCfg: {}
+    if (lodash.isArray(help.helpCfg)) {
+      custom = {
+        helpList: help.helpCfg,
+        helpCfg: {}
+      };
+    } else {
+      custom = help;
     }
-  } else {
-    custom = help
+
+    let helpConfig = lodash.defaults(diyCfg.helpCfg || {}, custom.helpCfg, sysCfg.helpCfg);
+    let helpList = diyCfg.helpList || custom.helpList || sysCfg.helpList;
+
+    let helpGroup = [];
+
+    lodash.forEach(helpList, (group) => {
+      if (group.auth && group.auth === 'master' && !e.isMaster) {
+        return true;
+      }
+
+      lodash.forEach(group.list, (help) => {
+        let icon = help.icon * 1;
+        if (!icon) {
+          help.css = 'display:none';
+        } else {
+          let x = (icon - 1) % 10;
+          let y = (icon - x - 1) / 10;
+          help.css = `background-position:-${x * 50}px -${y * 50}px`;
+        }
+      });
+
+      helpGroup.push(group);
+    });
+
+    let themeData = await HelpTheme.getThemeData(diyCfg.helpCfg || {}, sysCfg.helpCfg || {});
+
+    return await Common.render('help/index', {
+      helpCfg: helpConfig,
+      helpGroup,
+      ...themeData,
+      element: 'default'
+    }, { e, scale: 1.2 });
   }
 
-  let helpConfig = lodash.defaults(diyCfg.helpCfg || {}, custom.helpCfg, sysCfg.helpCfg)
-  let helpList = diyCfg.helpList || custom.helpList || sysCfg.helpList
-
-  let helpGroup = []
-
-  lodash.forEach(helpList, (group) => {
-    if (group.auth && group.auth === 'master' && !e.isMaster) {
-      return true
-    }
-
-    lodash.forEach(group.list, (help) => {
-      let icon = help.icon * 1
-      if (!icon) {
-        help.css = 'display:none'
-      } else {
-        let x = (icon - 1) % 10
-        let y = (icon - x - 1) / 10
-        help.css = `background-position:-${x * 50}px -${y * 50}px`
-      }
-    })
-
-    helpGroup.push(group)
-  })
-  let themeData = await HelpTheme.getThemeData(diyCfg.helpCfg || {}, sysCfg.helpCfg || {})
-  return await Common.render('help/index', {
-    helpCfg: helpConfig,
-    helpGroup,
-    ...themeData,
-    element: 'default'
-  }, { e, scale: 1.2 })
-}
-
-async versionInfo (e) {
-  return await Common.render('help/version-info', {
-    currentVersion: Version.version,
-    changelogs: Version.changelogs,
-    elem: 'anemo'
-  }, { e, scale: 1.2 })
-}
+  async versionInfo(e) {
+    return await Common.render('help/version-info', {
+      currentVersion: Version.version,
+      changelogs: Version.changelogs,
+      elem: 'anemo'
+    }, { e, scale: 1.2 });
+  }
 }

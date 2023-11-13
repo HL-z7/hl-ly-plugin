9d78aad36c84f1ced34fabc23c9270df8dbd84c7
落雨天阳 <923276093@qq.com>
2023-11-13 14:09:48 +0000

    把触发词改成厵神（ps:听狐狐讲原神太频繁了，他改成原神启动了，我觉得不妥，然后商讨决定把触发词改成厵神）
    Signed-off-by: 落雨天阳 <923276093@qq.com>

--- apps/ylnywys.js
+++ apps/ylnywys.js
@@ -10,7 +10,7 @@ export class example extends plugin {
       priority: -100000,
       rule: [
         {
-          reg: '原神启动',
+          reg: '厵神',
           fnc: 'yuanshen'
         }
       ]

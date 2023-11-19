43935d3338db282d11b55d80f50733d0bb38a627
狐璃 <zhilaohu114514@163.com>
2023-11-19 09:56:15 +0000

    update apps/列表 (9).js.
    Signed-off-by: 狐璃 <zhilaohu114514@163.com>

--- apps/列表 (9).js
+++ apps/列表 (9).js
@@ -21,7 +21,7 @@ export class example extends plugin {
   ps:使用的时候记得把q群和联系人改成自己的，如果出现一天指令两条消息请及时联系
   */
   async dggz (e) {
-    logger.info('[列表.js插件启动成功]')
+    logger.info('[hl-plugin]')
     let msg = "安卓所需软件\n"+
     "列如\n"+
     "ZeroTermux\n"+

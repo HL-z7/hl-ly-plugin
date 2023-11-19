5887fbd131244217a9e6edee7f86239976e0c34b
狐璃 <zhilaohu114514@163.com>
2023-11-19 09:51:31 +0000

    update apps/列表 (2).js.
    Signed-off-by: 狐璃 <zhilaohu114514@163.com>

--- apps/列表 (2).js
+++ apps/列表 (2).js
@@ -21,7 +21,7 @@ export class example extends plugin {
   ps:使用的时候记得把q群和联系人改成自己的，如果出现一天指令两条消息请及时联系
   */
   async dggz (e) {
-    logger.info('[列表.js插件启动成功]')
+    logger.info('[hl-plugin]')
     let msg = "我又来给你们推荐啦！\n"+
     "这次有纯 免 费 的喔！\n"+
     "https://xfree.netlify.app/\n"+

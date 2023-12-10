97ce3b18051b543b3f601cbd54eff51e38a8aee1
落雨天阳 <923276093@qq.com>
2023-12-10 10:13:31 +0000

    狐狐：菜就多练，以前是以前，现在是现在
    Signed-off-by: 落雨天阳 <923276093@qq.com>

--- apps/菜就多练.js
+++ apps/菜就多练.js
@@ -20,7 +20,6 @@ export class example extends plugin {
     logger.info('[hl-plugin]')
     let msg = "以前是以前，现在是现在"
     await this.e.reply(msg,true)
-    let path = ('plugins/hl-plugin/resources/MP3/菜就多练.mp3')
-    await this.e.reply(segment.video(path))
+    await this.reply(segment.video(`${_path}/resources/MP3/菜就多练.mp3`))
   }
 }

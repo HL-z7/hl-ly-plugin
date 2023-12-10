a7ddda9ab46c8dda7c01c5f4a1289e3cd507806a
落雨天阳 <923276093@qq.com>
2023-12-10 09:37:34 +0000

    update apps/菜就多练.js.
    Signed-off-by: 落雨天阳 <923276093@qq.com>

--- apps/菜就多练.js
+++ apps/菜就多练.js
@@ -20,7 +20,7 @@ export class example extends plugin {
     logger.info('[hl-plugin]')
     let msg = "以前是以前，现在是现在"
     await this.e.reply(msg,true)
-    let url = encodeURI(`https://gitee.com/luoyutianyang/tuchuan/raw/master/%E8%A7%86%E9%A2%91/%E8%8F%9C%E5%B0%B1%E5%A4%9A%E7%BB%83.mp4`)
+    let path = ('plugins/hl-plugin/resources/MP3/菜就多练.mp3')
     await this.e.reply(segment.video(url))
   }
 }

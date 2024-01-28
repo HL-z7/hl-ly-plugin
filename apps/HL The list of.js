200c8f9b53a899cfcb1737f1e65771abec7b59f4
狐璃 <zhilaohu114514@163.com>
2024-01-28 02:58:40 +0000

    新增功能
    Signed-off-by: 狐璃 <zhilaohu114514@163.com>

---
+++ apps/HL The list of.js
@@ -0,0 +1,38 @@
+import plugin from '../../lib/plugins/plugin.js'
+
+export class example extends plugin {
+  constructor () {
+    super({
+      name: 'HL列表',
+      dsc: '',
+      event: 'message',
+      priority: -114514,
+      rule: [
+        {
+          reg: '^(#|/)？hl列表$',
+          fnc: 'dggz'
+        }
+      ]
+    })
+  }
+  /*
+  作者QQ号:3610159055
+  作者:HL
+  ps:使用的时候记得把q群和联系人改成自己的，如果出现一天指令两条消息请及时联系
+  */
+  async dggz (e) {
+    logger.info('[HL-Plugin]')
+    let msg = "输入以下完整名称即可触发\n"+
+    "H随机三次元\n"+
+    "H随机柴郡\n"+
+    "H随机2次元\n"+
+    "H随机猫猫\n"+
+    "H随机原神\n"+
+    "H随机cos/Hcos\n"+
+    "H随机壁纸\n"+
+    "H随机甘城\n"+
+    "H心脏弱"
+    e.reply(msg,true)
+    return true
+  }
+}
---
+++ apps/随机.js
@@ -0,0 +1,104 @@
+import plugin from '../../lib/plugins/plugin.js'
+
+export class diaotu extends plugin {
+  constructor () {
+    super({
+      name: 'HL随机图片',
+      dsc: 'HL随机图片',
+      event: 'message',
+      priority: 6,
+      rule: [
+        {
+          reg: '^(#|/)?H随机三次元$',
+          fnc: 'scy'
+        },
+        {
+          reg: '^(#|/)?H随机柴郡$',
+          fnc: 'cj'
+        },
+        {
+          reg: '^(#|/)?H随机2次元$',
+          fnc: 'ecy'
+        },
+        {
+          reg: '^(#|/)?H随机猫猫$',
+          fnc: 'mm'
+        },
+        {
+          reg: '^(#|/)?H随机原神$',
+          fnc: 'ys'
+        },
+        {
+          reg: '^(#|/)?(H随机cos|H随机Cos|H随机COS|Hcos)$',
+          fnc: 'cos'
+        },
+        {
+          reg: '^(#|/)?H心脏弱$',
+          fnc: 'xzr'
+        },
+        {
+          reg: '^(#|/)?H随机甘城$',
+          fnc: 'gc'
+        },
+        {
+          reg: '^(#|/)?(H随机壁纸|H随机bz|Hhs)$',
+          fnc: 'bz'
+        }
+      ]
+    })
+  }
+
+  // 壁纸
+  async bz (e) {
+    await this.reply(segment.image('https://t.mwm.moe/pc'))
+    return true // 返回true 阻挡消息不再往下
+  }
+
+  // 原神
+  async ys (e) {
+    await this.reply(segment.image('https://api.lolimi.cn/API/yuan/?type=image'))
+    return true // 返回true 阻挡消息不再往下
+  }
+
+  // 猫猫
+  async mm (e) {
+    await this.reply(segment.image('https://api.suyanw.cn/api/mao'))
+    return true // 返回true 阻挡消息不再往下
+  }
+// 甘城
+  async mm (e) {
+    await this.reply(segment.image('https://guge123-mmapi.hf.space/mm'))
+    return true // 返回true 阻挡消息不再往下
+  }
+
+  
+  // 随机三次元
+  async scy (e) {
+    await this.reply(segment.image('https://api.vvhan.com/api/girl'))
+    return true // 返回true 阻挡消息不再往下
+  }
+
+  // 随机2次元
+  async ecy (e) {
+    await this.reply(segment.image('https://api.yimian.xyz/img'))
+    return true // 返回true 阻挡消息不再往下
+  }
+
+  // 随机柴郡
+  async cj (e) {
+    await this.reply(segment.image('https://guge123-chaiapi.hf.space/chai'))
+    return true // 返回true 阻挡消息不再往下
+  }
+
+// 心脏弱
+  async xzr (e) {
+    await this.reply(segment.image('https://guge123-xin.hf.space/xin'))
+    return true // 返回true 阻挡消息不再往下
+  }
+  
+  // 随机cos
+  async cos (e) {
+    await this.reply(segment.image('https://api.lolimi.cn/API/tup/xjj.php'))
+    return true // 返回true 阻挡消息不再往下
+  }
+}

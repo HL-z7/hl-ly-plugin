152406d82135ae9347714c7bb002176c98510f89
狐璃 <zhilaohu114514@163.com>
2024-03-11 14:46:00 +0000

    优化细节
    Signed-off-by: 狐璃 <zhilaohu114514@163.com>

--- guoba.support.js
+++ guoba.support.js
@@ -1,7 +1,7 @@
 import path from "path";
 import Config from "./components/Config.js";
 
-const _path = process.cwd() + "/plugins/hl-plugin";
+const _path = process.cwd() + "/plugins/hl-ly-plugin";
 
 /**
  *  支持锅巴配置
@@ -9,21 +9,21 @@ const _path = process.cwd() + "/plugins/hl-plugin";
 export function supportGuoba() {
     return {
         pluginInfo: {
-            name: "hl-plugin",
-            title: "hl-plugin",
+            name: "hl-ly-plugin",
+            title: "hl-ly-plugin",
             author: "@狐璃 @落雨天阳",
             authorLink: "https://gitee.com/fox-glaze",
-            link: "https://gitee.com/fox-glaze/hl-plugin",
+            link: "https://gitee.com/fox-glaze/hl-ly-plugin",
             isV3: true,
             isV2: false,
-            description: `hl-plugin`,
+            description: `hl-ly-plugin`,
             // 显示图标，此为个性化配置
             // 图标可在 https://icon-sets.iconify.design 这里进行搜索
             icon: "mdi:stove",
             // 图标颜色，例：#FF0000 或 rgb(255, 0, 0)
             iconColor: "#d19f56",
             // 如果想要显示成图片，也可以填写图标路径（绝对路径）
-            iconPath: path.join(_path, "img/hl-plugin.png"),
+            iconPath: path.join(_path, "img/hl-ly-plugin.png"),
         }
     }
 }

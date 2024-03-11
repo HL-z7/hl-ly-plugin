5c02e71fad53ee20cb9d8e9d20e4f7a67ee7347c
狐璃 <zhilaohu114514@163.com>
2024-03-11 14:40:19 +0000

    优化细节
    Signed-off-by: 狐璃 <zhilaohu114514@163.com>

--- mysyz/index.js
+++ mysyz/index.js
@@ -14,7 +14,7 @@ class Tools {
   }
 
   get Cfg () {
-    let file = './plugins/hl-plugin/config.yaml'
+    let file = './plugins/hl-ly-plugin/config.yaml'
     this._cfg = this._cfg || YAML.parse(fs.readFileSync(file, 'utf8'))
     return this._cfg
   }

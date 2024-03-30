b7c18ea4807791c6a227cbcd90151315c2f03fa0
狐璃 <zhilaohu114514@163.com>
2024-03-30 17:15:37 +0000

    删除文件 components/in.js

--- components/in.js
+++
@@ -1,7 +0,0 @@
-import Data from './Data.js'
-import Config from '././Config.js'
-import YamlReader from './YamlReader.js'
-const Path = process.cwd()
-const Plugin_Name = 'hy-ly-plugin'
-const Plugin_Path = `${Path}/plugins/${Plugin_Name}`
-export { Config, Data, Path, Plugin_Name, Plugin_Path, YamlReader }

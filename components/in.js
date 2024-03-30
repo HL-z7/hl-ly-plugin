eed8550df8362dd40d997110464c503299b1cbef
狐璃 <zhilaohu114514@163.com>
2024-03-30 14:44:40 +0000

    鸡你
    Signed-off-by: 狐璃 <zhilaohu114514@163.com>

--- components/in.js
+++ components/in.js
@@ -2,4 +2,4 @@ import Data from './Data.js'
 const Path = process.cwd()
 const Plugin_Name = 'hy-ly-plugin'
 const Plugin_Path = `${Path}/plugins/${Plugin_Name}`
-export { Config, Data, Version, Path, Plugin_Name, Plugin_Path, YamlReader }
+export { Data, Path, Plugin_Name, Plugin_Path, YamlReader }

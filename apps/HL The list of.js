b54a925a4cfa4fab6d4bffcb01ed08fb083c73f1
狐璃 <zhilaohu114514@163.com>
2024-05-02 06:49:12 +0000

    新增群发帮助
    Signed-off-by: 狐璃 <zhilaohu114514@163.com>

--- apps/HL The list of.js
+++ apps/HL The list of.js
@@ -11,6 +11,14 @@ export class example extends plugin {
         {
           reg: '^hl列表$',
           fnc: 'dggz'
+        },
+        {
+          reg: '^H群发帮助$',
+          fnc: 'hbz'
+        },
+        {
+          reg: '^(#HL插件|#hl插件)$',
+          fnc: 'hl'
         }
       ]
     })
@@ -21,7 +29,7 @@ export class example extends plugin {
   ps:使用的时候记得把q群和联系人改成自己的，如果出现一天指令两条消息请及时联系
   */
   async dggz (e) {
-    logger.info('[HL-Plugin]')
+    logger.info('[HL-LY-Plugin]')
     let msg = "输入以下完整名称即可触发\n"+
     "H随机三次元\n"+
     "随机柴郡\n"+
@@ -34,6 +42,23 @@ export class example extends plugin {
     "H随机甘城\n"+
     "H心脏弱"
     e.reply(msg,true)
+    return true
+  }
+
+async hbz (e) {
+    logger.info('[HL-LY-Plugin]')
+    let msg = "【一键群发帮助】\n"+
+    "一键群发+内容（只发送适配器为ICQQ）\n"+
+    "官bot群发+内容（只发送适配器为ICQQ与QQBot）\n"+
+    "单独群发#机器人QQ号#内容（指定QQ号执行群发）\n"+
+    "全局群发+内容（发送全平台）"
+    e.reply(msg,true)
+    return true
+  }
+
+  async hl (e) {
+    logger.info('[hl-ly-plugin]')
+    e.reply(`https://gitee.com/fox-glaze/hl-ly-plugin`)
     return true
   }
 }

7c91877e0373fd58ce699458c6e99bcebf59214d
狐璃 <zhilaohu114514@163.com>
2024-05-02 06:50:46 +0000

    新增【一键群发】多样式
    Signed-off-by: 狐璃 <zhilaohu114514@163.com>

--- apps/一键群发.js
+++ apps/一键群发.js
@@ -10,13 +10,25 @@ export class example extends plugin {
         {
           reg: '^一键群发(.*)$',
           fnc: 'cs'
+        },
+        {
+          reg: '^官bot群发(.*)$',
+          fnc: 'qqBotGroupSend'
+        },
+        {
+          reg: '^单独群发#(\\d+)#(.*)$',
+          fnc: 'sendToSingleGroup'
+        },
+        {
+          reg: '^全局群发(.*)$',
+          fnc: 'globalGroupSend'
         }
       ]
     })
   }
   
   async cs (e) {
-      if (!e.isMaster) {
+    if (!e.isMaster) {
       e.reply("你没有权限✘"); // 发送权限提示
       return false;
     }
@@ -40,5 +52,89 @@ export class example extends plugin {
     } else {
       return false;
     }
+  }
+  
+  async qqBotGroupSend (e) {
+    if (!e.isMaster) {
+      e.reply("你没有权限✘"); // 发送权限提示
+      return false;
+    }
+    if (this.e.adapter_name === 'QQBot') {
+      let textToTranslate = e.msg.replace(/官bot群发/g, "").trim();
+      if (textToTranslate === "") {
+        e.reply("请在指令后加上需要发送的内容");
+        return false;
+      }
+      
+      e.reply("正在执行官bot群发中...");
+      
+      for (const i of Bot.gl.keys()) {
+        await Bot.sleep(500); // 添加延迟
+        await Bot.pickGroup(i).sendMsg(textToTranslate);
+      }
+      return true;
+    } else if (this.e.adapter_name === 'ICQQ') {
+      e.reply("当前不支持官bot群发");
+      return false;
+    } else {
+      return false;
+    }
+  }
+  
+  async sendToSingleGroup(e) {
+    if (!e.isMaster) {
+      e.reply("你没有权限✘"); // 发送权限提示
+      return false;
+    }
+    let match = e.msg.match(/^单独群发#(\d+)#(.*)$/);
+    if (!match) {
+      e.reply("格式不正确，请使用 单独群发#id#内容 格式");
+      return false;
+    }
+    
+    let botQQ = parseInt(match[1]);
+    let textToTranslate = match[2];
+    
+    if (isNaN(botQQ) || botQQ <= 0) {
+      e.reply("机器人QQ号不正确，请输入正确的机器人QQ号");
+      return false;
+    }
+    
+    if (textToTranslate === "") {
+      e.reply("请输入要发送的内容");
+      return false;
+    }
+    
+    e.reply("正在执行单独群发中...");
+    
+    (async () => {
+      for (const [id,i] of Bot[botQQ].gl) {
+        await Bot[botQQ].pickGroup(id).sendMsg(textToTranslate);
+        await Bot.sleep(500);
+      }
+    })();
+    
+    return true;
+  }
+  
+  async globalGroupSend(e) {
+    if (!e.isMaster) {
+      e.reply("你没有权限✘"); // 发送权限提示
+      return false;
+    }
+    
+    let textToTranslate = e.msg.replace(/全局群发/g, "").trim();
+    if (textToTranslate === "") {
+      e.reply("请在指令后加上需要发送的内容");
+      return false;
+    }
+    
+    e.reply("正在执行全局群发中...");
+    
+    for (const i of Bot.gl.keys()) {
+      await Bot.pickGroup(i).sendMsg(textToTranslate);
+    }
+    
+    return true;
   }
 }

527d34139b6bc0809b208790404c53511f319a07
狐璃 <zhilaohu114514@163.com>
2024-05-09 10:40:01 +0000

    修复一下
    Signed-off-by: 狐璃 <zhilaohu114514@163.com>

--- apps/不可以这样子！.js
+++ apps/不可以这样子！.js
@@ -1,5 +1,6 @@
 import plugin from '../../../lib/plugins/plugin.js';
 import { segment } from "oicq";
+import { Config} from '../components/index.js'
 import fs from "fs";
 import path from "path";
 

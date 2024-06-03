08ec6b3fa60b66013b191bda5a3ad98cc3a5b9d1
狐璃 <zhilaohu114514@163.com>
2024-06-03 12:27:44 +0000

    个性化
    Signed-off-by: 狐璃 <zhilaohu114514@163.com>

--- index.js
+++ index.js
@@ -1,43 +1,55 @@
-//导入node:fs模块
-import fs from 'node:fs'
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 if (!global.segment) {
-  global.segment = (await import("oicq")).segment
+  const oicq = await import("oicq");
+  global.segment = oicq.segment;
 }
+
 const apps = {};
 global.hl_plugin = {
   apps: apps,
   puppeteer: null
 };
 
-//输出提示
-logger.mark('---------HL---------')
-logger.debug('\x1b[36m崽崽正在加载中...\x1b[0m')
-logger.info('\x1b[33mhl插件初始化~\x1b[0m')
-logger.mark('---------HL---------')
+// 输出提示
+logger.mark('---------HL---------');
+logger.debug('\x1b[36m崽崽正在加载中...\x1b[0m');
+logger.info('\x1b[33mHL插件初始化~\x1b[0m');
+logger.mark(`
+              ＿＿
+　　　　　／＞　　フ
+　　　　　|   _　 _l
+　 　　　／\` ミ＿꒳ノ
+　　 　 /　　　 　 |
+　　　 /　 ヽ　　 ﾉ
+　 　 │　　|　|　|
+　／￣|　　 |　|　|
+　| (￣ヽ＿_ヽ_)__)
+　＼二つ
+`);
+logger.mark('---------HL---------');
 
-//加载插件
-const files = fs.readdirSync('./plugins/hl-ly-plugin/apps').filter(file => file.endsWith('.js'))
+// 获取当前文件的目录名
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-let ret = []
+// 加载插件
+const pluginsDir = path.resolve(__dirname, './apps');
+const files = fs.readdirSync(pluginsDir).filter(file => file.endsWith('.js'));
 
-files.forEach((file) => {
-  ret.push(import(`./apps/${file}`))
-})
+const ret = await Promise.allSettled(files.map(file => import(`file://${path.join(pluginsDir, file)}`)));
 
+for (let i = 0; i < files.length; i++) {
+  const name = files[i].replace('.js', '');
 
-ret = await Promise.allSettled(ret)
-
-for (let i in files) {
-  let name = files[i].replace('.js', '')
-
-  if (ret[i].status != 'fulfilled') {
-      logger.error(`载入插件错误：${logger.red(name)}`)
-      logger.error(ret[i].reason)
-      continue
+  if (ret[i].status !== 'fulfilled') {
+    logger.error(`载入插件错误：${name}`);
+    logger.error(ret[i].reason);
+    continue;
   }
-  apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
+  apps[name] = ret[i].value[Object.keys(ret[i].value)[0]];
 }
 
-
-export { apps }
+export { apps };

df7054324f04ad7bae496cc9025d20dd037c470f
狐璃 <zhilaohu114514@163.com>
2024-06-24 17:07:17 +0000

    优化细节
    Signed-off-by: 狐璃 <zhilaohu114514@163.com>

---
+++ resources/help/theme/hl帮助/config.js
@@ -0,0 +1,10 @@
+export const style = {
+  "fontColor": "#140D02",
+  "fontShadow": "none",
+  "descColor": "#000000",
+  "contBgColor": "rgba(255, 255, 255, 0)",
+  "contBgBlur": 4,
+  "headerBgColor": "rgba(137, 185, 213, 0.25)",
+  "rowBgColor1": "rgba(255, 255, 255, 0)",
+  "rowBgColor2": "rgba(0, 0, 0, 0)"
+}

7af9992016cb052bbf5aab996e7e3d54a60265ff
狐璃 <zhilaohu114514@163.com>
2024-07-06 14:57:02 +0000

    新增运势
    Signed-off-by: 狐璃 <zhilaohu114514@163.com>

--- apps/运势.js
+++ apps/运势.js
@@ -4,25 +4,75 @@ export class jrys extends plugin {
       name: '今日运势',
       dsc: '运势',
       event: 'message',
-      priority: -114514,
+      priority: -1141145514,
       rule: [
         {
-          reg: '#运势',
+          reg: '^#?运势$',
           fnc: 'yunshi',
+        },
+        {
+          reg: '^#?逆天改命$',
+          fnc: 'changeFate',
         },
       ],
     });
   }
 
+  getMentionedInfo() {
+
+    const mention = this.e.message.find(msg => msg.type === 'at');
+
+    if (mention) {
+      return { qq: mention.qq, mentionUserId: mention.qq };
+    } else {
+      return { qq: this.e.user_id, mentionUserId: this.e.user_id };
+    }
+  }
+
   async yunshi() {
-    const url = 'https://image-api.api.luoyutianyang.icu/api/yunshi';
+    const { qq, mentionUserId } = this.getMentionedInfo();
+    const url = `http://jrys.api.zhilaohu.icu/?qq=${qq}&starRating=0&changeFate=false`;
     try {
       const response = await fetch(url);
       if (!response.ok) {
         throw new Error('API 访问错误');
       }
-      const content = await response.text();
-      await this.e.reply(`今日${content}`);
+      const data = await response.json();
+      if (data.code === 200 && data.status === 'Success') {
+        const content = data.data;
+        const reply = [
+          segment.at(mentionUserId), 
+          `\n【今日运势】\n\n运势总结: ${content.fortuneSummary}\n幸运星: ${content.luckyStar}\n签文: ${content.signText}\n解签: ${content.unSignText}`
+        ];
+        await this.e.reply(reply);
+      } else {
+        await this.e.reply('获取运势失败');
+      }
+    } catch (error) {
+      console.error(error);
+      await this.e.reply('API 访问出错');
+    }
+  }
+
+  async changeFate() {
+    const { qq, mentionUserId } = this.getMentionedInfo();
+    const url = `http://jrys.api.zhilaohu.icu/?qq=${qq}&starRating=7&changeFate=true`;
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('API 访问错误');
+      }
+      const data = await response.json();
+      if (data.code === 200 && data.status === 'Success') {
+        const content = data.data;
+        const reply = [
+          segment.at(mentionUserId), 
+          `\n【逆天改命】\n\n运势总结: ${content.fortuneSummary}\n幸运星: ${content.luckyStar}\n签文: ${content.signText}\n解签: ${content.unSignText}`
+        ];
+        await this.e.reply(reply);
+      } else {
+        await this.e.reply('获取运势失败');
+      }
     } catch (error) {
       console.error(error);
       await this.e.reply('API 访问出错');

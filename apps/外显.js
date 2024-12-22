83d9932aa6c73d340771bbc6dc2ede564d00b9c8
落雨天阳 <923276093@qq.com>
2024-12-22 14:23:34 +0000

    细节优化
    Signed-off-by: 落雨天阳 <923276093@qq.com>

---
+++ apps/外显.js
@@ -0,0 +1,44 @@
+import fs from 'fs';
+import path from 'path';
+
+const summaryFile = path.join('resources', 'summary.yaml');
+let summarymsg = fs.existsSync(summaryFile) ? fs.readFileSync(summaryFile, 'utf-8') : '[图片]';
+
+export class waixian extends plugin {
+  constructor() {
+    super({
+      name: '外显',
+      dsc: '外显',
+      event: 'message',
+      priority: -114514,
+      rule: [
+        {
+          reg: /^#?hl(设置|增加|删除|取消)外显(\s+)?(.+)?$/i,
+          fnc: 'waixian'
+        }
+      ]
+    });
+  }
+
+  async waixian(e) {
+    const [, action, , summary] = e.msg.match(this.rule[0].reg);
+    if (['设置','增加','添加'].includes(action)) {
+      summarymsg = summary;
+      fs.writeFileSync(summaryFile, summary);
+      return e.reply(`已经设置外显为: ${summary}`);
+  }
+  else if(['删除','取消'].includes(action)) {
+    summarymsg = '[图片]';
+    fs.writeFileSync(summaryFile, summarymsg);
+    return e.reply(`已经删除外显`);
+  }
+}
+}
+segment.image = (file, name) => {
+  return {
+    type: "image",
+    file,
+    name,
+    summary:summarymsg
+  };
+};

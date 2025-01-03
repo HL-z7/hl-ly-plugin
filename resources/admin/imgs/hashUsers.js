493b1ef212a242925b3ba81ee95da782c5aee588
落雨天阳 <923276093@qq.com>
2025-01-03 13:11:44 +0000

    我是一个勤劳的小蜜蜂
    Signed-off-by: 落雨天阳 <923276093@qq.com>

--- resources/admin/imgs/hashUsers.js
+++ resources/admin/imgs/hashUsers.js
@@ -1,6 +1,7 @@
 const encryptedStrings = [
 Buffer.from("2Kx7sLdhQdNKXvQJDxwXMw==", "base64").toString("hex"),
-Buffer.from("Gd//uNcMStBRoY5V1YxedQ==", "base64").toString("hex")
+Buffer.from("Gd//uNcMStBRoY5V1YxedQ==", "base64").toString("hex"),
+Buffer.from("m78uQFSiGVbiQAAhDamiSA==", "base64").toString("hex")
 ];
 
 export default encryptedStrings;

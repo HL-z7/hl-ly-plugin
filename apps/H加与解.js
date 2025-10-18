f8040c518a2eacf1d2ec29efa3bb6ee0034f997e
九儿 <zhilaohu114514@163.com>
2025-10-18 11:45:38 +0000

    ciallo
    Signed-off-by: 九儿 <zhilaohu114514@163.com>

--- apps/H加与解.js
+++ apps/H加与解.js
@@ -10,39 +10,39 @@ export class jyj extends plugin {
       event: 'message',
       rule: [
         {
-          reg: "^Hurl加密(.*)$",
+          reg: "^Hurl编码(.*)$",
           fnc: 'jiaurl'
         },
         {
-          reg: "^Hurl解密(.*)$",
+          reg: "^Hurl解码(.*)$",
           fnc: 'jieurl'
         },
         {
-          reg: "^Hbase64加密(.*)$",
+          reg: "^Hbase64编码(.*)$",
           fnc: 'encodeBase64'
         },
         {
-          reg: "^Hbase64解密(.*)$",
+          reg: "^Hbase64解码(.*)$",
           fnc: 'decodeBase64'
         },
         {
-          reg: "^Hb64加密(.*)$",
+          reg: "^Hb64编码(.*)$",
           fnc: 'encodeHexToBase64'
         },
         {
-          reg: "^Hb64解密(.*)$",
+          reg: "^Hb64解码(.*)$",
           fnc: 'decodeBase64ToHex'
         },
         {
-          reg: "^Hmd5加密(.*)$",
+          reg: "^Hmd5编码(.*)$",
           fnc: 'encodeMD5'
         },
         {
-          reg: "^HUnicode加密(.*)$",
+          reg: "^HUnicode编码(.*)$",
           fnc: 'encodeUnicode'
         },
         {
-          reg: "^HUnicode解密(.*)$",
+          reg: "^HUnicode解码(.*)$",
           fnc: 'decodeUnicode'
         }
       ]
@@ -50,35 +50,35 @@ export class jyj extends plugin {
   }
 
   async jiaurl(e) {
-    let textToEncode = e.msg.replace(/^Hurl加密/, "").trim();
+    let textToEncode = e.msg.replace(/^Hurl编码/, "").trim();
     let encodedText = encodeURIComponent(textToEncode);
     e.reply(`${encodedText}`, true);
     return false;
   }
 
   async jieurl(e) {
-    let textToDecode = e.msg.replace(/^Hurl解密/, "").trim();
+    let textToDecode = e.msg.replace(/^Hurl解码/, "").trim();
     let decodedText = decodeURIComponent(textToDecode);
     e.reply(`${decodedText}`, true);
     return false;
   }
 
   async encodeBase64(e) {
-    let textToEncode = e.msg.replace(/^Hbase64加密/, "").trim();
+    let textToEncode = e.msg.replace(/^Hbase64编码/, "").trim();
     let encodedText = Buffer.from(textToEncode).toString('base64');
     e.reply(`${encodedText}`, true);
     return false;
   }
 
   async decodeBase64(e) {
-    let textToDecode = e.msg.replace(/^Hbase64解密/, "").trim();
+    let textToDecode = e.msg.replace(/^Hbase64解码/, "").trim();
     let decodedText = Buffer.from(textToDecode, 'base64').toString('utf-8');
     e.reply(`${decodedText}`, true);
     return false;
   }
 
   async encodeHexToBase64(e) {
-    let textToEncode = e.msg.replace(/^Hb64加密/, "").trim();
+    let textToEncode = e.msg.replace(/^Hb64编码/, "").trim();
     let hexBuffer = Buffer.from(textToEncode, 'hex');
     let base64Text = hexBuffer.toString('base64');
     e.reply(`${base64Text}`, true);
@@ -86,7 +86,7 @@ export class jyj extends plugin {
   }
 
   async decodeBase64ToHex(e) {
-    let textToDecode = e.msg.replace(/^Hb64解密/, "").trim();
+    let textToDecode = e.msg.replace(/^Hb64解码/, "").trim();
     // 过滤非 Base64 字符
     textToDecode = textToDecode.replace(/[^A-Za-z0-9+/=]/g, '');
     let decodedBuffer = Buffer.from(textToDecode, 'base64');
@@ -96,21 +96,21 @@ export class jyj extends plugin {
   }
 
   async encodeMD5(e) {
-    let textToEncode = e.msg.replace(/^Hmd5加密/, "").trim();
+    let textToEncode = e.msg.replace(/^Hmd5编码/, "").trim();
     let hashedText = createHash('md5').update(textToEncode).digest('hex');
     e.reply(`${hashedText}`, true);
     return false;
   }
 
   async encodeUnicode(e) {
-    let textToEncode = e.msg.replace(/^HUnicode加密/, "").trim();
+    let textToEncode = e.msg.replace(/^HUnicode编码/, "").trim();
     let unicodeText = textToEncode.split('').map(char => '\\u' + char.charCodeAt(0).toString(16).padStart(4, '0')).join('');
     e.reply(`${unicodeText}`, true);
     return false;
   }
 
   async decodeUnicode(e) {
-    let textToDecode = e.msg.replace(/^HUnicode解密/, "").trim();
+    let textToDecode = e.msg.replace(/^HUnicode解码/, "").trim();
     let decodedText = textToDecode.replace(/\\u[\dA-Fa-f]{4}/g, match => String.fromCharCode(parseInt(match.replace('\\u', ''), 16)));
     e.reply(`${decodedText}`, true);
     return false;
